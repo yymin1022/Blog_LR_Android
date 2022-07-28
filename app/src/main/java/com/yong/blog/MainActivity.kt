29c922d340048fc42311931d36b1caa72f4b0b0d
yymin102@ <yymin1022@gmail.com>
2022-07-29 00:38:15 +0900

    Update PostListActivity.kt : Added Basic Compoose Elements

--- app/src/main/java/com/yong/blog/MainActivity.kt
+++ app/src/main/java/com/yong/blog/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.yong.blog.ui.theme.*
@@ -16,7 +18,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Blog_LR_AndroidTheme {
-                MainUI()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainUI()
+                }
             }
         }
     }
--- app/src/main/java/com/yong/blog/PostListActivity.kt
+++ app/src/main/java/com/yong/blog/PostListActivity.kt
@@ -3,10 +3,10 @@ package com.yong.blog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.yong.blog.ui.theme.Blog_LR_AndroidTheme
 
@@ -14,7 +14,7 @@ class PostListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val postType = intent.getStringExtra("postType")
+        val postType = intent.getStringExtra("postType").toString()
 
         setContent {
             Blog_LR_AndroidTheme {
@@ -22,9 +22,29 @@ class PostListActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text("PostView - Type is $postType")
+                    PostListUI(postType)
                 }
             }
         }
     }
+}
+
+@Composable
+fun PostListUI(postType: String) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("1인개발자 LR의 IT블로그") }
+            )
+        }
+    ) {
+        Column {
+            PostItemContainer(postType)
+        }
+    }
+}
+
+@Composable
+fun PostItemContainer(postType: String) {
+    Text("PostView - Type is $postType")
 }
