a0579d13f2131dc2791721f54e581adac39801b9
yymin102@ <yymin1022@gmail.com>
2022-08-07 22:55:29 +0900

    Update PostData.kt : Add Constructor, Function, Variables

--- app/src/main/java/com/yong/blog/API/PostData.kt
+++ app/src/main/java/com/yong/blog/API/PostData.kt
@@ -1,4 +1,13 @@
 package com.yong.blog.API
 
-class PostData {
+class PostData(var postID: String, var postType: String) {
+    var postDate = ""
+    var postIsPinned = false
+    var postTag = Array(1, {})
+    var postTitle = ""
+    var postURL = ""
+
+    fun getPostData(){
+        
+    }
 }
