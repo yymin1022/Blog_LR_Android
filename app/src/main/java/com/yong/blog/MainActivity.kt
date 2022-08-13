af0c071d5e28d0ba6121c872f46ba562ceb0bc2c
yymin1022 <yymin1022@gmail.com>
2022-08-13 15:32:28 +0900

    Update MainActivity.kt : Adjust Layout

--- app/src/main/java/com/yong/blog/MainActivity.kt
+++ app/src/main/java/com/yong/blog/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -45,7 +43,8 @@ fun MainUI() {
     ) {
         Column(
             modifier = Modifier
-                .padding(20.dp)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
             MainTitleContainer()
             MainButtonContainer()
