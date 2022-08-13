cbcda02e685422e869d1b5d37e5eff6b710df30d
yymin1022 <yymin1022@gmail.com>
2022-08-13 14:57:12 +0900

    Update Theme.kt : Disabled DarkMode Support

--- app/src/main/java/com/yong/blog/ui/theme/Theme.kt
+++ app/src/main/java/com/yong/blog/ui/theme/Theme.kt
@@ -1,16 +1,8 @@
 package com.yong.blog.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = BlogBlue500,
-    primaryVariant = BlogBlue700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = BlogBlue500,
@@ -29,17 +21,10 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun Blog_LR_AndroidTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
