a7af14e3874aa3af1c64df17d0dbf3177c9cfc33
yymin102@ <yymin1022@gmail.com>
2022-08-14 02:03:07 +0900

    Update PostViewActivity.kt : Adjust Post Title Alignment

--- app/src/main/java/com/yong/blog/PostViewActivity.kt
+++ app/src/main/java/com/yong/blog/PostViewActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.yong.blog.api.API
@@ -140,7 +141,8 @@ fun PostViewTitle(postTitle: String) {
         text = postTitle,
         style = TextStyle(
             fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
         )
     )
 }
