8d5eb76b44df11c713a23b558f24932f2a89c9a8
yymin1022 <yymin1022@gmail.com>
2022-08-15 02:03:48 +0900

    Update Files : Removed Markwon Library

--- app/src/main/java/com/yong/blog/PostViewActivity.kt
+++ app/src/main/java/com/yong/blog/PostViewActivity.kt
@@ -1,7 +1,6 @@
 package com.yong.blog
 
 import android.os.Bundle
-import android.text.Spanned
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,14 +22,8 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.yong.blog.api.API
 import com.yong.blog.api.PostData
 import com.yong.blog.ui.theme.Blog_LR_AndroidTheme
-import io.noties.markwon.Markwon
-import io.noties.markwon.html.HtmlPlugin
-import io.noties.markwon.syntax.Prism4jThemeDarkula
-import io.noties.markwon.syntax.SyntaxHighlightPlugin
-import io.noties.prism4j.Prism4j
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.commonmark.node.Node
 
 class PostViewActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -120,14 +113,6 @@ fun PostViewContent(postContent: String) {
             context -> TextView(context)
         }
     ) {
-        val prism4j = Prism4j(PostGrammarLocator())
-        val markwon: Markwon = Markwon.builder(ctx)
-            .usePlugin(HtmlPlugin.create())
-            .usePlugin(SyntaxHighlightPlugin.create(prism4j, Prism4jThemeDarkula.create(), String()))
-            .build()
-        val mdNode: Node = markwon.parse(postContent)
-        val mdSpanned: Spanned = markwon.render(mdNode)
-        markwon.setParsedMarkdown(it, mdSpanned)
         it.setTextColor(Color.Black.hashCode())
     }
 }
